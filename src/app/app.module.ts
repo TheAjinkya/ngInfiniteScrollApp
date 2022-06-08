7fccc30f9a08c7597980762224e1d215a5bfb66e
TheAjinkya <ajinkya.chanshetty@gmail.com>
2022-06-08 20:16:02 +0530

    updated the list with infinite scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InfiniteScrollModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
